20711e30bec263289dcc5023d073167739d331b2
BruceSherrill <47867604+BruceSherrill@users.noreply.github.com>
2019-10-31 13:01:32 -0500

    First iteration of the trie structure.
    Trie methods:
    -Trie()
    -insert(String)
    -importDictionary()
    -isWord(String)
    
    Driver
    
    Dictionary.txt

---
+++ Boggle/src/Driver.java
@@ -0,0 +1,19 @@
+
+public class Driver 
+{
+    public static void main(String args[]) 
+    {
+    	
+    	//Creates the trie
+    	Trie tree = new Trie();
+    	
+    	//Imports the dictionary text file to the trie
+    	tree.importDictionary();
+    	
+    	//Tests to see if the word is valid
+    	System.out.println(tree.isWord("hello"));
+    	System.out.println(tree.isWord("goodbye"));
+    	System.out.println(tree.isWord("notaword"));
+
+	}
+}
