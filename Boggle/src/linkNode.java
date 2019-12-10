cfe5ba5b37de077f895212601a0caa3705a247dd
EmmaGustavson <47867986+EmmaGustavson@users.noreply.github.com>
2019-12-09 20:31:47 -0600

    Update linkNode.java
    Added comments to methods

--- Boggle/src/linkNode.java
+++ Boggle/src/linkNode.java
@@ -1,14 +1,30 @@
 
 public class linkNode {
 
+	 
+	/** Action performed:
+		 * Parameters: 
+		 * Precondition: 
+		 * PostCondition:
+		 * Throws: None
+		 */
+	
+	//Basic node varibles
 	private String data;
 	private linkNode link;
 	
+	/** Action perform: Overriden Constructor that creates a node
+	 * Parameters: A String and a linkNode
+	 * Precondition: None
+	 * PostCondition: Newly created linkNode object made
+	 * Throws: None
+	 */
 	public linkNode(String data, linkNode link) {
 		this.data = data;
 		this.link = link;
 	}
 	
+	//Getters and setters
 	public String getData() {
 		return data;
 	}
@@ -25,19 +41,24 @@ public class linkNode {
 		this.link = link;
 	}
 	
-	
+	/** Action performed: Will add a node after previous one in list
+	 * Parameters: A String containing node data 
+	 * Precondition: None
+	 * PostCondition: A new node is added to link list
+	 * Throws: None
+	 */
 	public void addNodeAfter(String data) {
 		link = new linkNode(data, link);
 	}
 	
+	/** Action performed: Will remove a node from link list
+	 * Parameters: None
+	 * Precondition: None
+	 * PostCondition: A node will be removed from the link list
+	 * Throws: None
+	 */
 	public void removeNodeAfter() {
 		link = link.link;
-	}
-	
-	public static void print(linkNode head) {
-		
-		
-	
 	}
 	
 }
